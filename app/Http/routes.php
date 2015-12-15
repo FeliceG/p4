223fe283ccb3a58399fc60a97c932d975f7e67d4
Felice Gardner <felice.gardner@gmail.com>
2015-12-15 17:55:34 -0500

    changed debugbar to see if production db working

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -96,7 +96,7 @@ Route::get('/debug', function() {
     echo '<h1>Debugging?</h1>';
     if(config('app.debug')) echo "Yes"; else echo "No";
 
-//    echo '<h1>Database Config</h1>';
+    echo '<h1>Database Config</h1>';
     /*
     The following line will output your MySQL credentials.
     Uncomment it only if you're having a hard time connecting to the database and you
@@ -104,18 +104,18 @@ Route::get('/debug', function() {
     When you're done debugging, comment it back out so you don't accidentally leave it
     running on your live server, making your credentials public.
     */
-    //print_r(config('database.connections.mysql'));
+  print_r(config('database.connections.mysql'));
 
-//    echo '<h1>Test Database Connection</h1>';
-//    try {
-//        $results = DB::select('SHOW DATABASES;');
-//        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-//        echo "<br><br>Your Databases:<br><br>";
-//        print_r($results);
-//    }
-//    catch (Exception $e) {
-//        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-//    }
+   echo '<h1>Test Database Connection</h1>';
+    try {
+       $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    }
+   catch (Exception $e) {
+       echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+         }
 
     echo '</pre>';
 
